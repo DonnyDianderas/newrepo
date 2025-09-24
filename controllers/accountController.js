a362ffa5f1734f9527727229cc5494619bf72ecb
DonnyDianderas <donnydianderas@gmail.com>
2025-09-24 00:40:11 -0500

    completed Data Validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -15,6 +15,7 @@ async function buildLogin(req, res, next) {
   res.render("account/login", {
     title: "Login",
     nav,
+    errors: null,
   })
 }
 
@@ -26,7 +27,7 @@ async function buildRegister(req, res, next) {
   res.render("account/register", {
     title: "Register",
     nav,
-    errors: null
+    errors: null,
   })
 }
 
@@ -52,12 +53,17 @@ async function registerAccount(req, res) {
     res.status(201).render("account/login", {
       title: "Login",
       nav,
+      errors: null
     })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
     res.status(501).render("account/register", {
       title: "Registration",
       nav,
+      /*errors: null,
+      account_firstname,
+      account_lastname,
+      account_email*/
     })
   }
 }
